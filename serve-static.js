5f02babcf1fe4343a4be0895a954a7cd0d9f838f
Jean-pierre Vincent <jpvirtual@yahoo.com>
2020-11-01 18:29:56 +0100

    le serveur marche avec HTTP/2 si on est en Node 14

--- serve-static.js
+++ serve-static.js
@@ -3,55 +3,70 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const favicon = require('serve-favicon')
-
+const port = 8181
+// Hors TP: serveur de fichiers statiques
+app.get('/', (req, res) => res.sendFile('./index.html', { root: './' }))
+app.use('/demos', express.static('demos/'))
+app.use('/tp', express.static('tp-ecommerce/'))
+app.use(favicon(__dirname + '/rocket.png'))
 
 // 3. on allume la compression pour tous les fichiers texte
-app.use(require('compression')( /*{level:9}*/ )) // les taux de compression élevés posent des problèmes aux machines faibles
+app.use(require('compression')(/*{level:9}*/)) // les taux de compression élevés posent des problèmes aux machines faibles
 /*app.use(require('shrink-ray')({
   brotli: {
     quality: 14
   }
 }))*/
 
-app.use(express.static('demos/'))
-app.use(favicon(__dirname + '/rocket.png'))
-
 // fonts.gstatic.com
 
-
-app.use(express.static('page-tp/', {
-  lastModified: true,
-  etag: true,
-  // 4. démo de pré-connection à un domaine que le navigateur ne peut pas deviner juste en lisant le HTML
-  // attention à la démo sur webpagetest : notre WPT privé n'affiche pas les requêtes vers google s'il est en httpS
-  /*setHeaders: function (res, path, stat) {
+app.use(
+	express.static('tp/', {
+		lastModified: true,
+		etag: true,
+		// 4. démo de pré-connection à un domaine que le navigateur ne peut pas deviner juste en lisant le HTML
+		// attention à la démo sur webpagetest : notre WPT privé n'affiche pas les requêtes vers google s'il est en httpS
+		/*setHeaders: function (res, path, stat) {
     res.set('Link', '<https://fonts.gstatic.com>; rel=preconnect;crossorigin;')
   }*/
-  setHeaders: function (res, path, stat) {
-    //res.set('Link', '<http://fonts.gstatic.com>; rel=preconnect;crossorigin;')
-    res.set('Link', '<http://fonts.gstatic.com/s/amaranth/v9/KtkuALODe433f0j1zMnFHdA.woff2>; rel=preload; as=font')
-  }
-}))
+		setHeaders: function (res, path, stat) {
+			//res.set('Link', '<http://fonts.gstatic.com>; rel=preconnect;crossorigin;')
+			res.set(
+				'Link',
+				'<http://fonts.gstatic.com/s/amaranth/v9/KtkuALODe433f0j1zMnFHdA.woff2>; rel=preload; as=font'
+			)
+		},
+	})
+)
 // 1. on met un cache client d'un an pour toutes les requêtes
-app.use(express.static('page-tp/', {
-  maxAge: '1y' // = 1 an
-}))
+app.use(
+	express.static('tp/', {
+		maxAge: '1y', // = 1 an
+	})
+)
 // app
-app.listen(8181, _ => console.log('server opened on port 8181'))
+// app.listen(port, (_) => console.log('server opened on port ' + port))
 // 2. test avec HTTP/2. (PASSER EN HTTPS!)
-//require('spdy') // HTTP/2*/
-/*require('https') // HTTPS 1.1
-  .createServer({
-    key: fs.readFileSync(path.join(__dirname, '/node_modules/http2/example/localhost.key')),
-    cert: fs.readFileSync(path.join(__dirname, '/node_modules/http2/example/localhost.crt'))
-  }, app)
-  .listen(8181, _ => console.log('HTTPS server opened on port 8181'))
-*/
-// NodeJS le supporte maintenant nativement : https://nodejs.org/api/http2.html#http2_http2stream_pushstream_headers_options_callback 
+require('spdy') // HTTP/2*/
+	//require('https') // HTTPS 1.1
+	.createServer(
+		{
+			key: fs.readFileSync(
+				path.join(__dirname, '/node_modules/http2/example/localhost.key')
+			),
+			cert: fs.readFileSync(
+				path.join(__dirname, '/node_modules/http2/example/localhost.crt')
+			),
+		},
+		app
+	)
+	.listen(port, (_) => console.log('HTTPS server opened on port ' + port))
+
+// NodeJS le supporte maintenant nativement : https://nodejs.org/api/http2.html#http2_http2stream_pushstream_headers_options_callback
 // mais il n'assure pas la rétro-compatibilité, donc Express plante avec : https://github.com/expressjs/express/issues/3388
 /*require('http2').createSecureServer({
     key: fs.readFileSync(path.join(__dirname, '/node_modules/http2/example/localhost.key')),
     cert: fs.readFileSync(path.join(__dirname, '/node_modules/http2/example/localhost.crt'))
   }, app)
-  .listen(8181, _ => console.log('HTTP2 server opened on port 8181'))
+  .listen(port, _ => console.log('HTTP2 server opened on port '+port))
   */
