3bac83f2cd697dbfb43dec74a42533fb29c73d86
Jean-pierre Vincent <jpvirtual@yahoo.com>
2020-11-13 10:22:08 +0100

    Utilisation de React.lazy

--- tp-webpack/app/app.js
+++ tp-webpack/app/app.js
@@ -1,21 +1,22 @@
-import loadable from '@loadable/component'
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+
 // import DatePicker from './component/date-picker.js'
-const DatePicker = loadable(() =>
+const DatePicker = React.lazy(() =>
 	import(/* webpackChunkName: "date-picker" */ './component/date-picker.js')
 )
 
+import loadable from '@loadable/component'
 // import Autocomplete from './component/autocomplete.js'
 const Autocomplete = loadable(() =>
 	import(/* webpackChunkName: "autocomplete" */ './component/autocomplete.js')
 )
 
-import React from 'react'
-
-import ReactDOM from 'react-dom'
-
 ReactDOM.render(
 	<>
-		<DatePicker />
+		<Suspense fallback={<div>Chargement…</div>}>
+			<DatePicker />
+		</Suspense>
 		<h4 className='h3'>Then pick a language</h4>
 		<Autocomplete />
 	</>,
